36cf74dde1adb04557e226a5ecf1739bd7082948
Ryan Florence <rpflorence@gmail.com>
2024-02-07 09:08:00 -0700

    first

---
+++ app/root.tsx
@@ -0,0 +1,44 @@
+import "./bootstrap.css";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Root!</h1>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function HydrateFallback() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <p>Loading...</p>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/_index.tsx
@@ -0,0 +1,33 @@
+import "../index.css";
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix SPA" },
+    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Welcome to Remix (SPA Mode)</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/future/spa-mode"
+            rel="noreferrer"
+          >
+            SPA Mode Guide
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
